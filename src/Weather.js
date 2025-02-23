f065da0cecf899e199c63e13922370634ba1f919
liflatt <lflatt1221@gmail.com>
2025-02-23 12:35:33 -0600

    Added target =" _blank"  and rel = "noopener noreferrer" to links

--- src/Weather.js
+++ src/Weather.js
@@ -154,11 +154,33 @@ export default function WeatherApp() {
 
       <footer className="border-top pt-3 text-center">
         <p>
-          Coded by <a href="https://github.com/liflatt">Lindsey Flatt</a>,
-          open-sourced
-          <a href="https://github.com/liflatt/react-weather-app"> on GitHub</a>,
-          and
-          <a href="https://lf-weather.netlify.app/"> hosted on Netlify</a>.
+          Coded by{" "}
+          <a
+            href="https://github.com/liflatt"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Lindsey Flatt
+          </a>
+          , open-sourced
+          <a
+            href="https://github.com/liflatt/react-weather-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {" "}
+            on GitHub
+          </a>
+          , and
+          <a
+            href="https://react-weather-app-by-lindsey-flatt.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {" "}
+            hosted on Netlify
+          </a>
+          .
         </p>
       </footer>
     </div>
